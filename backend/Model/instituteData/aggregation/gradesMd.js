c46b006861527271057d01effd23b535f28d6297
Devnotes2 <devnotes24@gmail.com>
2024-12-05 05:14:07 +0100

    Update gradesMd.js

--- backend/Model/instituteData/aggregation/gradesMd.js
+++ backend/Model/instituteData/aggregation/gradesMd.js
@@ -15,11 +15,11 @@ const GradesSchema = new Schema({
     required: true
   },
   gradeDuration: {
-    type: Number,  // Duration of the grade (e.g., "1 year")
+    type: mongoose.Schema.Types.ObjectId,  // Duration of the grade (e.g., "1 year")
     required: true
   },
   isElective: {
-    type: Number,  // Whether the grade is elective or not
+    type: mongoose.Schema.Types.ObjectId,  // Whether the grade is elective or not
     required: true
   }
 });  // Optionally, you can use timestamps to track creation and update times
