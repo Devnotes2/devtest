74613c67abee64d8107e69743649950273a5aaeb
SACHIN V B <sachinvb27@gmail.com>
2025-05-13 22:50:14 +0530

    feat: Update location types schema to remove required fields for flexibility in data entry

--- backend/Model/instituteData/aggregation/locationTypesInInstituteMd.js
+++ backend/Model/instituteData/aggregation/locationTypesInInstituteMd.js
@@ -3,11 +3,11 @@ const { Schema } = mongoose;
 
 // Define the schema for individual location types in institutes
 const locationTypesInInstituteSchema = new Schema({
-  instituteId: { type: Schema.Types.ObjectId, ref: 'Institute', required: true }, // Reference to Institute collection
-  locationTypes: { type: Schema.Types.ObjectId, required: true }, // Reference to location type
-  capacity: { type: Number, required: true },
-  description: { type: String, required: true },
-  location: { type: String, required: true },
+  instituteId: { type: Schema.Types.ObjectId, ref: 'Institute',  }, // Reference to Institute collection
+  locationTypes: { type: Schema.Types.ObjectId,  }, // Reference to location type
+  capacity: { type: Number, },
+  description: { type: String, },
+  location: { type: String, },
 }, { collection: 'locationTypesInInstitute' }); // Use a new collection
 
 const createLocationTypesInInstituteModel = (connection) => {
