36afb8393760154dedaec24148cdf170eb53f2d8
SACHIN V B <sachinvb27@gmail.com>
2025-06-14 01:50:52 +0530

    feat: Update institute schema to make district field required and add timestamps option

--- backend/Model/instituteData/institutesMd.js
+++ backend/Model/instituteData/institutesMd.js
@@ -6,14 +6,14 @@ const instituteSchema = new Schema({
   instituteName: { type: String, required: true },
   address: { type: String, required: true },
   city: { type: String, required: true },
-  district: { type: String },
+  district: { type: String,required:true },
   state: { type: String, required: true },
   country: { type: String, required: true },
   pinCode: { type: Number, required: true },
   contactNo1: { type: String, required: true },
   contactNo2: { type: String },
   emailId: { type: String, required: true },
-}, { collection: 'instituteData' }); // Specify the collection name
+}, { collection: 'instituteData' },{ timestamps: true }); // Specify the collection name
 
 // Create the model
 const createInstitutesModel = (connection) => {
